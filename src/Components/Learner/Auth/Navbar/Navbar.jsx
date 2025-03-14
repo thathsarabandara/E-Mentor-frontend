195751bb972ddecf9cf747e9f41e7f3fdf7e6546
thathsarabandara <thathsaraarumapperuma@gmail.com>
2025-03-15 02:39:53 +0530

    E-mentor day 5.1

--- src/Components/Learner/Auth/Navbar/Navbar.jsx
+++ src/Components/Learner/Auth/Navbar/Navbar.jsx
@@ -1,57 +1,51 @@
-import React, { useState } from 'react'
-import student from '../../../../assets/images/student/profile.jpeg'
-import { FaArrowRight } from 'react-icons/fa'
+import React from 'react';
+import student from '../../../../assets/images/student/profile.jpeg';
+import { FaArrowRight } from 'react-icons/fa';
+import { SlUserFollow } from 'react-icons/sl';
 
 function Navbar() {
-    const [isOpen, setOpen] = useState();
-    
   return (
     <>
       <div className='flex flex-col justify-center items-center my-8'>
         <div className='relative w-full h-64 bg-orange-100'>
-            <div className='absolute bottom-0 inset-x-72 w-/12 bg-white flex justify-start items-center py-3 pt-5 px-8 rounded-t-lg'>
-                <div className='w-24 h-24 rounded-full overflow-hidden'>
-                    <img className='transform transition duration-300 easy-in-out hover:scale-125' src={student} alt='' />
-                </div>
-                <div className='flex flex-col justify-center items-start ml-12 font-inter mr-36'>
-                    <p className='text-xl font-bold'>
-                        Kevin Gilbert
-                    </p>
-                    <p className='text-sm text-gray-600'>
-                        Web Designer & Best-Selling Instructor
-                    </p>
-                </div>
-                <a href='/' className='ml-96 flex justify-center items-center bg-orange-100 py-4 px-5 font-inter text-orange-500 text-sm'>
-                    Become Instructor
-                    <FaArrowRight  className='ml-3'/>
-                </a>
+          <div className='absolute bottom-0 inset-x-0 mx-auto max-w-5xl bg-white flex flex-col md:flex-row justify-between items-center py-3 pt-5 px-6 md:px-8 rounded-t-lg'>
+            <div className='flex items-center space-x-4'>
+              <div className='w-20 h-20 md:w-24 md:h-24 rounded-full overflow-hidden'>
+                <img className='transform transition duration-300 ease-in-out hover:scale-110' src={student} alt='Profile' />
+              </div>
+              <div className='flex flex-col justify-center'>
+                <p className='text-sm md:text-lg font-bold'>Kevin Gilbert</p>
+                <p className='text-xs md:text-sm text-gray-600'>Web Designer & Best-Selling Instructor</p>
+              </div>
             </div>
+            <a href='/' className='flex justify-center items-center bg-orange-100 py-3 px-4 rounded-md text-orange-500 text-sm mt-3 md:mt-0'>
+              <SlUserFollow />
+            </a>
+          </div>
         </div>
-        <div className='relative bg-white w-full h-8'>
-            <ul className='absolute top-0 inset-x-72 w-/12 bg-white flex justify-between items-center px-10 py-3 text-sm'>
-                <li>
-                    <a href="/" className="hover:text-orange-400 font-inter hover:border-b-2 border-orange-500 py-4 active:border-t-2">Dashboard</a>
+        <div className='relative bg-white w-full h-auto overflow-x-auto shadow-sm'>
+          <div className='whitespace-nowrap scrollbar-hide'>
+            <ul className='flex justify-start md:justify-center space-x-6 px-6 md:px-10 py-3 text-sm md:text-base'>
+              {[
+                { name: 'Dashboard', path: '/' },
+                { name: 'Courses', path: '/courses' },
+                { name: 'Teachers', path: '/about' },
+                { name: 'Wishlist', path: '/contact' },
+                { name: 'Purchase History', path: '/career' },
+                { name: 'Settings', path: '/beaninstructor' },
+              ].map((item, index) => (
+                <li key={index} className='px-2 md:px-4'>
+                  <a href={item.path} className='hover:text-orange-400 font-inter hover:border-b-2 border-orange-500 py-2 md:py-4'>
+                    {item.name}
+                  </a>
                 </li>
-                <li>
-                    <a href="/courses" className="hover:text-orange-400 font-inter hover:border-b-2 border-orange-500 py-4 active:border-t-2">Courses</a>
-                </li>
-                <li>
-                    <a href="/about" className="hover:text-orange-400 font-inter hover:border-b-2 border-orange-500 py-4 active:border-t-2">Teachers</a>
-                </li>
-                <li>
-                    <a href="/contact" className="hover:text-orange-400 font-inter hover:border-b-2 border-orange-500 py-4 active:border-t-2">Wishlist</a>
-                </li>
-                <li>
-                    <a href="/career" className="hover:text-orange-400 font-inter hover:border-b-2 border-orange-500 py-4 active:border-t-2">Purchase History</a>
-                </li>
-                <li>
-                    <a href="/beaninstructor" className="hover:text-orange-400 font-inter hover:border-b-2 border-orange-500 py-4 active:border-t-2">Settings</a>
-                </li>
+              ))}
             </ul>
+          </div>
         </div>
       </div>
     </>
-  )
+  );
 }
 
-export default Navbar
+export default Navbar;
