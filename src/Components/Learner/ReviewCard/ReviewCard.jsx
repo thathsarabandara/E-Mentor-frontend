7c4e91d2675f04b6807c6b56b6766fadf1dac843
thathsarabandara <thathsaraarumapperuma@gmail.com>
2025-03-23 13:36:22 +0530

    E-mentor day 8.4

--- src/Components/Learner/ReviewCard/ReviewCard.jsx
+++ src/Components/Learner/ReviewCard/ReviewCard.jsx
@@ -10,9 +10,10 @@ function ReviewCard({ rating = 4.8, totalStars = 5, starData = {} }) {
   }, {});
 
   return (
-    <div className='flex justify-center items-center'>
-      <div className='flex flex-col justify-center items-center border py-5 px-8 mr-6'>
-        <p className='text-5xl mb-5'>{rating}</p>
+    <div className='flex flex-col lg:flex-row justify-center items-center lg:space-x-6 w-full'>
+      {/* Rating Display */}
+      <div className='flex flex-col justify-center items-center border py-5 px-8 lg:px-10 mr-0 lg:mr-6 w-full max-w-xs lg:max-w-md'>
+        <p className='text-4xl sm:text-5xl mb-4'>{rating}</p>
         <div className='flex justify-center items-center space-x-2 text-orange-500 mb-2'>
           {[...Array(totalStars)].map((_, index) => (
             <FaStar key={index} className={index < Math.round(rating) ? 'text-orange-500' : 'text-orange-300'} />
@@ -20,7 +21,9 @@ function ReviewCard({ rating = 4.8, totalStars = 5, starData = {} }) {
         </div>
         <p className='text-sm'>Course Rating</p>
       </div>
-      <div className='flex flex-col justify-center items-start'>
+
+      {/* Review Breakdown */}
+      <div className='flex flex-col justify-center items-start w-full'>
         {starRatings.map((star) => (
           <ReviewCardRow key={star} rating={star} totalStars={totalStars} percentage={percentages[star]} />
         ))}
